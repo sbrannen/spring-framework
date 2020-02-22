775ce2fa88e6e72b2a2c289a6c744a33bec2deeb
Sam Brannen <sbrannen@pivotal.io>
2020-02-22 14:48:31 +0100

    Include main resources, test fixtures, and Kotlin in native image

--- spring-core/setcp.sh
+++ spring-core/setcp.sh
@@ -1,3 +1,11 @@
 export CP=`cat cp.txt | grep "^/" | tr '\n' ':'`
 export CP=$CP:junit-platform-console-standalone-1.6.0.jar
-export CP=$CP:./build/classes/java/test:./build/classes/java/main:./build/resources/test
+
+# Everything:
+export CP=$CP:./build/resources/test:./build/resources/main:./build/classes/java/test:./build/classes/kotlin/test:./build/classes/java/testFixtures:./build/classes/java/main:./build/classes/kotlin/main
+
+## Everyting minus Kotlin test classes:
+# export CP=$CP:./build/resources/test:./build/classes/java/test:./build/classes/java/testFixtures:./build/classes/java/main:./build/classes/kotlin/main
+
+## Everyting minus Kotlin test classes and Kotlin main classes (i.e., Kotlin coroutine support):
+# export CP=$CP:./build/resources/test:./build/classes/java/test:./build/classes/java/testFixtures:./build/classes/java/main
