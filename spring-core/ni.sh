958b1af84844262c030a3f17e0107bb6381f7f2f
Sam Brannen <sbrannen@pivotal.io>
2020-02-22 15:31:41 +0100

    Overhaul GraalVM native image configuration

--- spring-core/ni.sh
+++ spring-core/ni.sh
@@ -1,25 +1,28 @@
 native-image \
-  --no-server \
-  -Dverbose=true \
-  -H:+TraceClassInitialization \
-  -H:Name=build/spring-core-tests.bin \
-  -H:+ReportExceptionStackTraces \
-  --no-fallback \
--Dlog4j2.disable.jmx=true \
---initialize-at-build-time=org.springframework.core.SerializableTypeWrapper\$SerializableTypeProxy,org.apache.logging.log4j.core.LoggerContext,org.apache.logging.log4j.core.config,org.apache.logging.log4j.core.lookup,org.apache.logging.log4j.status,org.apache.logging.log4j.util,org.apache.logging.log4j.Level,org.apache.logging.log4j.core.util,org.apache.logging.log4j.message,org.apache.logging.log4j,org.apache.logging.log4j.MarkerManager\$Log4jMarker,org.apache.logging.log4j.core.impl,org.apache.logging.log4j.status,org.apache.logging.log4j.status.StatusLogger,org.apache.logging.log4j.spi.DefaultThreadContextMap,org.apache.logging.log4j.message.AbstractMessageFactory,org.apache.logging.log4j.core.selector.ClassLoaderContextSelector,org.apache.logging.log4j.core.config.LoggerConfig,org.apache.logging.log4j.simple.SimpleLogger,org.apache.logging.log4j.MarkerManager,org.apache.logging.log4j.message.ParameterizedNoReferenceMessageFactory \
---initialize-at-build-time=org.springframework.core.type.AnnotationMetadataTests\$SubclassEnum\$1 \
---initialize-at-build-time=org.springframework.core.type.AnnotationMetadataTests\$SubclassEnum\$2 \
-  --allow-incomplete-classpath \
-  --report-unsupported-elements-at-runtime \
-  -cp $CP:graal \
-  org.junit.platform.console.ConsoleLauncher
+	--no-server \
+	--no-fallback \
+	--allow-incomplete-classpath \
+	--report-unsupported-elements-at-runtime \
+	--initialize-at-build-time=org.apache.logging.log4j.core.LoggerContext,org.apache.logging.log4j.core.config,org.apache.logging.log4j.core.lookup,org.apache.logging.log4j.status,org.apache.logging.log4j.util,org.apache.logging.log4j.Level,org.apache.logging.log4j.core.util,org.apache.logging.log4j.message,org.apache.logging.log4j,org.apache.logging.log4j.MarkerManager\$Log4jMarker,org.apache.logging.log4j.core.impl,org.apache.logging.log4j.status,org.apache.logging.log4j.status.StatusLogger,org.apache.logging.log4j.spi.DefaultThreadContextMap,org.apache.logging.log4j.message.AbstractMessageFactory,org.apache.logging.log4j.core.selector.ClassLoaderContextSelector,org.apache.logging.log4j.core.config.LoggerConfig,org.apache.logging.log4j.simple.SimpleLogger,org.apache.logging.log4j.MarkerManager,org.apache.logging.log4j.message.ParameterizedNoReferenceMessageFactory \
+	--initialize-at-build-time=org.apache.logging.log4j.spi.ExtendedLogger \
+	--initialize-at-build-time=org.springframework.core.SerializableTypeWrapper\$SerializableTypeProxy \
+	--initialize-at-build-time=org.springframework.core.type.AnnotationMetadataTests\$SubclassEnum\$1,org.springframework.core.type.AnnotationMetadataTests\$SubclassEnum\$2 \
+	--initialize-at-run-time=org.apache.commons.logging.LogAdapter,org.apache.commons.logging.LogAdapter\$Log4jLog \
+	--initialize-at-run-time=org.apache.logging.log4j.core.async.AsyncLoggerConfigDisruptor \
+	--initialize-at-run-time=org.apache.logging.log4j.core.async.AsyncLoggerContext \
+	--initialize-at-run-time=org.apache.logging.log4j.core.config.yaml.YamlConfiguration \
+	--initialize-at-run-time=org.apache.logging.log4j.core.pattern.JAnsiTextRenderer \
+	-H:+TraceClassInitialization \
+	-H:+ReportExceptionStackTraces \
+	-H:+AddAllCharsets \
+	-H:Name=build/spring-core-tests.bin \
+	-Dverbose=true \
+	-Dlog4j2.disable.jmx=true \
+	-cp $CP:build/graalvm \
+	org.junit.platform.console.ConsoleLauncher
 
-# -H:ReflectionConfigurationResources=graal/reflect-config.json \
 exit
---initialize-at-build-time=org.apache.commons.logging.LogAdapter \
-  -H:ReflectionConfigurationResources=graal/aspects-reflect-config.json \
 
-  -H:ResourceConfigurationFiles=graal/resource-config.json \
-  -H:DynamicProxyConfigurationFiles=graal/proxies.json \
-
-
+	--verbose \
+	--initialize-at-run-time=net.bytebuddy,net.bytebuddy.description.method,net.bytebuddy.implementation,net.bytebuddy.implementation.bind,net.bytebuddy.implementation.bind.annotation,net.bytebuddy.implementation.bind.annotation.Argument\$BindingMechanic,net.bytebuddy.implementation.bind.annotation.Super,net.bytebuddy.implementation.bind.annotation.Super\$Instantiation \
+	-H:DynamicProxyConfigurationFiles=proxy-config.json \
