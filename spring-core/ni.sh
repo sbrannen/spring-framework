aba75926b41634376a6da1ca83fc9139ed87505b
Sam Brannen <sbrannen@pivotal.io>
2020-02-25 15:12:05 +0100

    Consolidate Log4j native image config

--- spring-core/ni.sh
+++ spring-core/ni.sh
@@ -3,15 +3,11 @@ native-image \
 	--no-fallback \
 	--allow-incomplete-classpath \
 	--report-unsupported-elements-at-runtime \
-	--initialize-at-build-time=org.apache.logging.log4j.core.LoggerContext,org.apache.logging.log4j.core.config,org.apache.logging.log4j.core.lookup,org.apache.logging.log4j.status,org.apache.logging.log4j.util,org.apache.logging.log4j.Level,org.apache.logging.log4j.core.util,org.apache.logging.log4j.message,org.apache.logging.log4j,org.apache.logging.log4j.MarkerManager\$Log4jMarker,org.apache.logging.log4j.core.impl,org.apache.logging.log4j.status,org.apache.logging.log4j.status.StatusLogger,org.apache.logging.log4j.spi.DefaultThreadContextMap,org.apache.logging.log4j.message.AbstractMessageFactory,org.apache.logging.log4j.core.selector.ClassLoaderContextSelector,org.apache.logging.log4j.core.config.LoggerConfig,org.apache.logging.log4j.simple.SimpleLogger,org.apache.logging.log4j.MarkerManager,org.apache.logging.log4j.message.ParameterizedNoReferenceMessageFactory \
-	--initialize-at-build-time=org.apache.logging.log4j.spi.ExtendedLogger \
+	--initialize-at-build-time=org.apache.logging.log4j.core.LoggerContext,org.apache.logging.log4j.core.config,org.apache.logging.log4j.core.lookup,org.apache.logging.log4j.status,org.apache.logging.log4j.util,org.apache.logging.log4j.Level,org.apache.logging.log4j.core.util,org.apache.logging.log4j.message,org.apache.logging.log4j,org.apache.logging.log4j.MarkerManager\$Log4jMarker,org.apache.logging.log4j.core.impl,org.apache.logging.log4j.status,org.apache.logging.log4j.status.StatusLogger,org.apache.logging.log4j.spi.DefaultThreadContextMap,org.apache.logging.log4j.spi.ExtendedLogger,org.apache.logging.log4j.message.AbstractMessageFactory,org.apache.logging.log4j.core.selector.ClassLoaderContextSelector,org.apache.logging.log4j.core.config.LoggerConfig,org.apache.logging.log4j.simple.SimpleLogger,org.apache.logging.log4j.MarkerManager,org.apache.logging.log4j.message.ParameterizedNoReferenceMessageFactory \
 	--initialize-at-build-time=org.springframework.core.SerializableTypeWrapper\$SerializableTypeProxy \
 	--initialize-at-build-time=org.springframework.core.type.AnnotationMetadataTests\$SubclassEnum\$1,org.springframework.core.type.AnnotationMetadataTests\$SubclassEnum\$2 \
+	--initialize-at-run-time=org.apache.logging.log4j.core.async.AsyncLoggerConfigDisruptor,org.apache.logging.log4j.core.async.AsyncLoggerContext,org.apache.logging.log4j.core.config.yaml.YamlConfiguration,org.apache.logging.log4j.core.pattern.JAnsiTextRenderer \
 	--initialize-at-run-time=org.apache.commons.logging.LogAdapter,org.apache.commons.logging.LogAdapter\$Log4jLog \
-	--initialize-at-run-time=org.apache.logging.log4j.core.async.AsyncLoggerConfigDisruptor \
-	--initialize-at-run-time=org.apache.logging.log4j.core.async.AsyncLoggerContext \
-	--initialize-at-run-time=org.apache.logging.log4j.core.config.yaml.YamlConfiguration \
-	--initialize-at-run-time=org.apache.logging.log4j.core.pattern.JAnsiTextRenderer \
 	-H:+TraceClassInitialization \
 	-H:+ReportExceptionStackTraces \
 	-H:+AddAllCharsets \
