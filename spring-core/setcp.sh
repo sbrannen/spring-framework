12ce3b5482bbf8dda79fef9c371e23ee86f2699c
Sam Brannen <sbrannen@pivotal.io>
2020-02-29 16:53:27 +0100

    Add dependency on JUnit Platform ConsoleLauncher

--- spring-core/setcp.sh
+++ spring-core/setcp.sh
@@ -1,11 +1,2 @@
 export CP=`cat cp.txt | grep "^/" | tr '\n' ':'`
-export CP=$CP:junit-platform-console-standalone-1.6.0.jar
-
-# Everything:
-export CP=$CP:./build/resources/test:./build/resources/main:./build/classes/java/test:./build/classes/kotlin/test:./build/classes/java/testFixtures:./build/classes/java/main:./build/classes/kotlin/main
-
-## Everyting minus Kotlin test classes:
-# export CP=$CP:./build/resources/test:./build/classes/java/test:./build/classes/java/testFixtures:./build/classes/java/main:./build/classes/kotlin/main
-
-## Everyting minus Kotlin test classes and Kotlin main classes (i.e., Kotlin coroutine support):
-# export CP=$CP:./build/resources/test:./build/classes/java/test:./build/classes/java/testFixtures:./build/classes/java/main
+export CP=$CP:./build/resources/test:./build/resources/testFixtures:./build/resources/main:./build/classes/java/test:./build/classes/kotlin/test:./build/classes/java/testFixtures:./build/classes/java/main:./build/classes/kotlin/main
