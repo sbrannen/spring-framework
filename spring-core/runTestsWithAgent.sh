7cf9c48d32c3f6fd245b137d5cb6cac24982c55b
Sam Brannen <sbrannen@pivotal.io>
2020-03-01 17:02:22 +0100

    Introduce custom testWithGraalVmAgent Gradle task
    This Gradle task replaces the runTestsWithAgent.sh script.

--- spring-core/runTestsWithAgent.sh
+++
@@ -1,4 +0,0 @@
-CP=`cat ./build/test_classpath.txt`
-rm -rf build/graalvm/META-INF
-mkdir -p build/graalvm/META-INF/native-image
-java -agentlib:native-image-agent=config-output-dir=build/graalvm/META-INF/native-image -Dorg.graalvm.nativeimage.imagecode=agent -cp $CP org.junit.platform.console.ConsoleLauncher --details=summary --exclude-tag="uses-blockhound | uses-kotlin-coroutines | uses-kotlin-reflection | uses-mockito | uses-mockk" @./build/test_classes.txt
