293cc975167ab4d2dab1c9585429ea44487077d2
Sam Brannen <sbrannen@pivotal.io>
2020-03-02 14:21:26 +0100

    Introduce custom testInNativeImage Gradle task
    This Gradle task replaces the runTestsInNativeImage.sh script.

--- spring-core/runTestsInNativeImage.sh
+++
@@ -1,1 +0,0 @@
-./build/native-image-tests.bin --details=summary --exclude-tag="uses-blockhound | uses-kotlin-coroutines | uses-kotlin-reflection | uses-mockito | uses-mockk" @./build/test_classes.txt
