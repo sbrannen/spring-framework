997ad7856f5e89025888b3cb186ee675b706e776
Sam Brannen <sbrannen@vmware.com>
2020-12-15 23:08:14 +0100

    Polish ApplicationEventsTestExecutionListener
    See gh-25616

--- spring-test/src/main/java/org/springframework/test/context/event/ApplicationEventsTestExecutionListener.java
+++ spring-test/src/main/java/org/springframework/test/context/event/ApplicationEventsTestExecutionListener.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.core.Conventions;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestContextAnnotationUtils;
 import org.springframework.test.context.support.AbstractTestExecutionListener;
@@ -36,6 +37,16 @@ import org.springframework.util.Assert;
 public class ApplicationEventsTestExecutionListener extends AbstractTestExecutionListener {
 
 	/**
+	 * Attribute name for a {@link TestContext} attribute which indicates
+	 * whether the test class for the given test context is annotated with
+	 * {@link RecordApplicationEvents @RecordApplicationEvents}.
+	 * <p>Permissible values include {@link Boolean#TRUE} and {@link Boolean#FALSE}.
+	 */
+	private static final String RECORD_APPLICATION_EVENTS = Conventions.getQualifiedAttributeName(
+			ApplicationEventsTestExecutionListener.class, "recordApplicationEvents");
+
+
+	/**
 	 * Returns {@code 1800}.
 	 */
 	@Override
@@ -45,15 +56,15 @@ public class ApplicationEventsTestExecutionListener extends AbstractTestExecutio
 
 	@Override
 	public void prepareTestInstance(TestContext testContext) throws Exception {
-		if (recordingApplicationEvents(testContext)) {
-			registerListenerAndResolvableDependency(testContext);
+		if (recordApplicationEvents(testContext)) {
+			registerListenerAndResolvableDependencyIfNecessary(testContext.getApplicationContext());
 			ApplicationEventsHolder.registerApplicationEvents();
 		}
 	}
 
 	@Override
 	public void beforeTestMethod(TestContext testContext) throws Exception {
-		if (recordingApplicationEvents(testContext)) {
+		if (recordApplicationEvents(testContext)) {
 			// Register a new ApplicationEvents instance for the current thread
 			// in case the test instance is shared -- for example, in TestNG or
 			// JUnit Jupiter with @TestInstance(PER_CLASS) semantics.
@@ -63,25 +74,25 @@ public class ApplicationEventsTestExecutionListener extends AbstractTestExecutio
 
 	@Override
 	public void afterTestMethod(TestContext testContext) throws Exception {
-		if (recordingApplicationEvents(testContext)) {
+		if (recordApplicationEvents(testContext)) {
 			ApplicationEventsHolder.unregisterApplicationEvents();
 		}
 	}
 
-	private boolean recordingApplicationEvents(TestContext testContext) {
-		return TestContextAnnotationUtils.hasAnnotation(testContext.getTestClass(), RecordApplicationEvents.class);
+	private boolean recordApplicationEvents(TestContext testContext) {
+		return testContext.computeAttribute(RECORD_APPLICATION_EVENTS, name ->
+				TestContextAnnotationUtils.hasAnnotation(testContext.getTestClass(), RecordApplicationEvents.class));
 	}
 
 	@SuppressWarnings("resource")
-	private void registerListenerAndResolvableDependency(TestContext testContext) {
-		ApplicationContext applicationContext = testContext.getApplicationContext();
+	private void registerListenerAndResolvableDependencyIfNecessary(ApplicationContext applicationContext) {
 		Assert.isInstanceOf(AbstractApplicationContext.class, applicationContext,
 				"The ApplicationContext for the test must be an AbstractApplicationContext");
 		AbstractApplicationContext aac = (AbstractApplicationContext) applicationContext;
-		boolean alreadyRegistered = aac.getApplicationListeners().stream()
+		boolean notAlreadyRegistered = aac.getApplicationListeners().stream()
 				.map(Object::getClass)
-				.anyMatch(ApplicationEventsApplicationListener.class::equals);
-		if (!alreadyRegistered) {
+				.noneMatch(ApplicationEventsApplicationListener.class::equals);
+		if (notAlreadyRegistered) {
 			// Register a new ApplicationEventsApplicationListener.
 			aac.addApplicationListener(new ApplicationEventsApplicationListener());
 
