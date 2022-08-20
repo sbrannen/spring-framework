e0d7a184ff9ddfef47b0adc9124b728db356d030
Sam Brannen <sbrannen@vmware.com>
2022-08-20 13:54:28 +0200

    Reorganize code in DefaultCacheAwareContextLoaderDelegate

--- spring-test/src/main/java/org/springframework/test/context/cache/DefaultCacheAwareContextLoaderDelegate.java
+++ spring-test/src/main/java/org/springframework/test/context/cache/DefaultCacheAwareContextLoaderDelegate.java
@@ -73,40 +73,6 @@ public class DefaultCacheAwareContextLoaderDelegate implements CacheAwareContext
 		this.contextCache = contextCache;
 	}
 
-	/**
-	 * Get the {@link ContextCache} used by this context loader delegate.
-	 */
-	protected ContextCache getContextCache() {
-		return this.contextCache;
-	}
-
-	/**
-	 * Load the {@code ApplicationContext} for the supplied merged context configuration.
-	 * <p>Supports both the {@link SmartContextLoader} and {@link ContextLoader} SPIs.
-	 * @throws Exception if an error occurs while loading the application context
-	 */
-	@SuppressWarnings("deprecation")
-	protected ApplicationContext loadContextInternal(MergedContextConfiguration mergedContextConfiguration)
-			throws Exception {
-
-		ContextLoader contextLoader = mergedContextConfiguration.getContextLoader();
-		Assert.notNull(contextLoader, "Cannot load an ApplicationContext with a NULL 'contextLoader'. " +
-				"Consider annotating your test class with @ContextConfiguration or @ContextHierarchy.");
-
-		ApplicationContext applicationContext;
-
-		if (contextLoader instanceof SmartContextLoader smartContextLoader) {
-			applicationContext = smartContextLoader.loadContext(mergedContextConfiguration);
-		}
-		else {
-			String[] locations = mergedContextConfiguration.getLocations();
-			Assert.notNull(locations, "Cannot load an ApplicationContext with a NULL 'locations' array. " +
-					"Consider annotating your test class with @ContextConfiguration or @ContextHierarchy.");
-			applicationContext = contextLoader.loadContext(locations);
-		}
-
-		return applicationContext;
-	}
 
 	@Override
 	public boolean isContextLoaded(MergedContextConfiguration mergedContextConfiguration) {
@@ -150,6 +116,41 @@ public class DefaultCacheAwareContextLoaderDelegate implements CacheAwareContext
 		synchronized (this.contextCache) {
 			this.contextCache.remove(mergedContextConfiguration, hierarchyMode);
 		}
+	}
+
+	/**
+	 * Get the {@link ContextCache} used by this context loader delegate.
+	 */
+	protected ContextCache getContextCache() {
+		return this.contextCache;
+	}
+
+	/**
+	 * Load the {@code ApplicationContext} for the supplied merged context configuration.
+	 * <p>Supports both the {@link SmartContextLoader} and {@link ContextLoader} SPIs.
+	 * @throws Exception if an error occurs while loading the application context
+	 */
+	@SuppressWarnings("deprecation")
+	protected ApplicationContext loadContextInternal(MergedContextConfiguration mergedContextConfiguration)
+			throws Exception {
+
+		ContextLoader contextLoader = mergedContextConfiguration.getContextLoader();
+		Assert.notNull(contextLoader, "Cannot load an ApplicationContext with a NULL 'contextLoader'. " +
+				"Consider annotating your test class with @ContextConfiguration or @ContextHierarchy.");
+
+		ApplicationContext applicationContext;
+
+		if (contextLoader instanceof SmartContextLoader smartContextLoader) {
+			applicationContext = smartContextLoader.loadContext(mergedContextConfiguration);
+		}
+		else {
+			String[] locations = mergedContextConfiguration.getLocations();
+			Assert.notNull(locations, "Cannot load an ApplicationContext with a NULL 'locations' array. " +
+					"Consider annotating your test class with @ContextConfiguration or @ContextHierarchy.");
+			applicationContext = contextLoader.loadContext(locations);
+		}
+
+		return applicationContext;
 	}
 
 }
