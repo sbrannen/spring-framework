sbrannen/spring-framework

commit 375e0e6827216ceb38e9bcf0b97b46bb79d79de6

Handle Content-Length in ShallowEtagHeaderFilter more robustly
This commit ensures that setting the Content-Length through
setHeader("Content-Length", x") has the same effect as calling
setContentLength in the ShallowEtagHeaderFilter. It also filters out
Content-Type headers similarly to Content-Length.

Closes gh-32039

spring-web/src/main/java/org/springframework/web/util/ContentCachingResponseWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,10 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.WriteListener;
@@ -54,6 +58,9 @@ public class ContentCachingResponseWrapper extends HttpServletResponseWrapper {
 
 	@Nullable
 	private Integer contentLength;
+
+	@Nullable
+	private String contentType;
 
 
 	/**
@@ -140,6 +147,122 @@ public class ContentCachingResponseWrapper extends HttpServletResponseWrapper {
 	}
 
 	@Override
+	public void setContentType(String type) {
+		this.contentType = type;
+	}
+
+	@Override
+	@Nullable
+	public String getContentType() {
+		return this.contentType;
+	}
+
+	@Override
+	public boolean containsHeader(String name) {
+		if (HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
+			return this.contentLength != null;
+		}
+		else if (HttpHeaders.CONTENT_TYPE.equalsIgnoreCase(name)) {
+			return this.contentType != null;
+		}
+		else {
+			return super.containsHeader(name);
+		}
+	}
+
+	@Override
+	public void setHeader(String name, String value) {
+		if (HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
+			this.contentLength = Integer.valueOf(value);
+		}
+		else if (HttpHeaders.CONTENT_TYPE.equalsIgnoreCase(name)) {
+			this.contentType = value;
+		}
+		else {
+			super.setHeader(name, value);
+		}
+	}
+
+	@Override
+	public void addHeader(String name, String value) {
+		if (HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
+			this.contentLength = Integer.valueOf(value);
+		}
+		else if (HttpHeaders.CONTENT_TYPE.equalsIgnoreCase(name)) {
+			this.contentType = value;
+		}
+		else {
+			super.addHeader(name, value);
+		}
+	}
+
+	@Override
+	public void setIntHeader(String name, int value) {
+		if (HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
+			this.contentLength = Integer.valueOf(value);
+		}
+		else {
+			super.setIntHeader(name, value);
+		}
+	}
+
+	@Override
+	public void addIntHeader(String name, int value) {
+		if (HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
+			this.contentLength = Integer.valueOf(value);
+		}
+		else {
+			super.addIntHeader(name, value);
+		}
+	}
+
+	@Override
+	@Nullable
+	public String getHeader(String name) {
+		if (HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
+			return (this.contentLength != null) ? this.contentLength.toString() : null;
+		}
+		else if (HttpHeaders.CONTENT_TYPE.equalsIgnoreCase(name)) {
+			return this.contentType;
+		}
+		else {
+			return super.getHeader(name);
+		}
+	}
+
+	@Override
+	public Collection<String> getHeaders(String name) {
+		if (HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
+			return this.contentLength != null ? Collections.singleton(this.contentLength.toString()) :
+					Collections.emptySet();
+		}
+		else if (HttpHeaders.CONTENT_TYPE.equalsIgnoreCase(name)) {
+			return this.contentType != null ? Collections.singleton(this.contentType) : Collections.emptySet();
+		}
+		else {
+			return super.getHeaders(name);
+		}
+	}
+
+	@Override
+	public Collection<String> getHeaderNames() {
+		Collection<String> headerNames = super.getHeaderNames();
+		if (this.contentLength != null || this.contentType != null) {
+			List<String> result = new ArrayList<>(headerNames);
+			if (this.contentLength != null) {
+				result.add(HttpHeaders.CONTENT_LENGTH);
+			}
+			if (this.contentType != null) {
+				result.add(HttpHeaders.CONTENT_TYPE);
+			}
+			return result;
+		}
+		else {
+			return headerNames;
+		}
+	}
+
+	@Override
 	public void setBufferSize(int size) {
 		if (size > this.content.size()) {
 			this.content.resize(size);
@@ -197,11 +320,17 @@ public class ContentCachingResponseWrapper extends HttpServletResponseWrapper {
 	protected void copyBodyToResponse(boolean complete) throws IOException {
 		if (this.content.size() > 0) {
 			HttpServletResponse rawResponse = (HttpServletResponse) getResponse();
-			if ((complete || this.contentLength != null) && !rawResponse.isCommitted()) {
-				if (rawResponse.getHeader(HttpHeaders.TRANSFER_ENCODING) == null) {
-					rawResponse.setContentLength(complete ? this.content.size() : this.contentLength);
+			if (!rawResponse.isCommitted()) {
+				if (complete || this.contentLength != null) {
+					if (rawResponse.getHeader(HttpHeaders.TRANSFER_ENCODING) == null) {
+						rawResponse.setContentLength(complete ? this.content.size() : this.contentLength);
+					}
+					this.contentLength = null;
 				}
-				this.contentLength = null;
+				if (complete || this.contentType != null) {
+					rawResponse.setContentType(this.contentType);
+					this.contentType = null;
+				}
 			}
 			this.content.writeTo(rawResponse.getOutputStream());
 			this.content.reset();

spring-web/src/test/java/org/springframework/web/filter/ShallowEtagHeaderFilterTests.java
@@ -23,6 +23,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.http.MediaType;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.testfixture.servlet.MockHttpServletRequest;
 import org.springframework.web.testfixture.servlet.MockHttpServletResponse;
@@ -68,6 +69,7 @@ class ShallowEtagHeaderFilterTests {
 		FilterChain filterChain = (filterRequest, filterResponse) -> {
 			assertThat(filterRequest).as("Invalid request passed").isEqualTo(request);
 			((HttpServletResponse) filterResponse).setStatus(HttpServletResponse.SC_OK);
+			filterResponse.setContentType(MediaType.TEXT_PLAIN_VALUE);
 			FileCopyUtils.copy(responseBody, filterResponse.getOutputStream());
 		};
 		filter.doFilter(request, response, filterChain);
@@ -75,6 +77,7 @@ class ShallowEtagHeaderFilterTests {
 		assertThat(response.getStatus()).as("Invalid status").isEqualTo(200);
 		assertThat(response.getHeader("ETag")).as("Invalid ETag").isEqualTo("\"0b10a8db164e0754105b7a99be72e3fe5\"");
 		assertThat(response.getContentLength()).as("Invalid Content-Length header").isGreaterThan(0);
+		assertThat(response.getContentType()).as("Invalid Content-Type header").isEqualTo(MediaType.TEXT_PLAIN_VALUE);
 		assertThat(response.getContentAsByteArray()).as("Invalid content").isEqualTo(responseBody);
 	}
 
@@ -88,6 +91,7 @@ class ShallowEtagHeaderFilterTests {
 		FilterChain filterChain = (filterRequest, filterResponse) -> {
 			assertThat(filterRequest).as("Invalid request passed").isEqualTo(request);
 			((HttpServletResponse) filterResponse).setStatus(HttpServletResponse.SC_OK);
+			filterResponse.setContentType(MediaType.TEXT_PLAIN_VALUE);
 			FileCopyUtils.copy(responseBody, filterResponse.getOutputStream());
 		};
 		filter.doFilter(request, response, filterChain);
@@ -95,6 +99,7 @@ class ShallowEtagHeaderFilterTests {
 		assertThat(response.getStatus()).as("Invalid status").isEqualTo(200);
 		assertThat(response.getHeader("ETag")).as("Invalid ETag").isEqualTo("W/\"0b10a8db164e0754105b7a99be72e3fe5\"");
 		assertThat(response.getContentLength()).as("Invalid Content-Length header").isGreaterThan(0);
+		assertThat(response.getContentType()).as("Invalid Content-Type header").isEqualTo(MediaType.TEXT_PLAIN_VALUE);
 		assertThat(response.getContentAsByteArray()).as("Invalid content").isEqualTo(responseBody);
 	}
 
@@ -108,14 +113,16 @@ class ShallowEtagHeaderFilterTests {
 		FilterChain filterChain = (filterRequest, filterResponse) -> {
 			assertThat(filterRequest).as("Invalid request passed").isEqualTo(request);
 			byte[] responseBody = "Hello World".getBytes(StandardCharsets.UTF_8);
-			FileCopyUtils.copy(responseBody, filterResponse.getOutputStream());
 			filterResponse.setContentLength(responseBody.length);
+			filterResponse.setContentType(MediaType.TEXT_PLAIN_VALUE);
+			FileCopyUtils.copy(responseBody, filterResponse.getOutputStream());
 		};
 		filter.doFilter(request, response, filterChain);
 
 		assertThat(response.getStatus()).as("Invalid status").isEqualTo(304);
 		assertThat(response.getHeader("ETag")).as("Invalid ETag").isEqualTo("\"0b10a8db164e0754105b7a99be72e3fe5\"");
 		assertThat(response.containsHeader("Content-Length")).as("Response has Content-Length header").isFalse();
+		assertThat(response.containsHeader("Content-Type")).as("Response has Content-Type header").isFalse();
 		byte[] expecteds = new byte[0];
 		assertThat(response.getContentAsByteArray()).as("Invalid content").isEqualTo(expecteds);
 	}

commit c26ef7898fe882525478bea4e0cd393718162a3b

Honor Content-[Type|Length] headers from wrapped response again
Commit 375e0e6827 introduced a regression in
ContentCachingResponseWrapper (CCRW). Specifically, CCRW no longer
honors Content-Type and Content-Length headers that have been set on
the wrapped response and returns null for those header values if they
have not been set directly on the CCRW.

This commit fixes this regression as follows.

- The Content-Type and Content-Length headers set in the wrapped
  response are honored in getContentType(), containsHeader(),
  getHeader(), and getHeaders() unless those headers have been set
  directly on the CCRW.

- In copyBodyToResponse(), the Content-Type in the wrapped response is
  only overridden if the the Content-Type has been set directly on the
  CCRW.

See gh-32039
Closes gh-32317

spring-web/src/main/java/org/springframework/web/util/ContentCachingResponseWrapper.java
@@ -43,6 +43,7 @@ import org.springframework.util.FastByteArrayOutputStream;
  * <p>Used e.g. by {@link org.springframework.web.filter.ShallowEtagHeaderFilter}.
  *
  * @author Juergen Hoeller
+ * @author Sam Brannen
  * @since 4.1.3
  * @see ContentCachingRequestWrapper
  */
@@ -157,16 +158,19 @@ public class ContentCachingResponseWrapper extends HttpServletResponseWrapper {
 	@Override
 	@Nullable
 	public String getContentType() {
-		return this.contentType;
+		if (this.contentType != null) {
+			return this.contentType;
+		}
+		return super.getContentType();
 	}
 
 	@Override
 	public boolean containsHeader(String name) {
-		if (HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
-			return this.contentLength != null;
+		if (this.contentLength != null && HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
+			return true;
 		}
-		else if (HttpHeaders.CONTENT_TYPE.equalsIgnoreCase(name)) {
-			return this.contentType != null;
+		else if (this.contentType != null && HttpHeaders.CONTENT_TYPE.equalsIgnoreCase(name)) {
+			return true;
 		}
 		else {
 			return super.containsHeader(name);
@@ -222,10 +226,10 @@ public class ContentCachingResponseWrapper extends HttpServletResponseWrapper {
 	@Override
 	@Nullable
 	public String getHeader(String name) {
-		if (HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
-			return (this.contentLength != null) ? this.contentLength.toString() : null;
+		if (this.contentLength != null && HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
+			return this.contentLength.toString();
 		}
-		else if (HttpHeaders.CONTENT_TYPE.equalsIgnoreCase(name)) {
+		else if (this.contentType != null && HttpHeaders.CONTENT_TYPE.equalsIgnoreCase(name)) {
 			return this.contentType;
 		}
 		else {
@@ -235,12 +239,11 @@ public class ContentCachingResponseWrapper extends HttpServletResponseWrapper {
 
 	@Override
 	public Collection<String> getHeaders(String name) {
-		if (HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
-			return this.contentLength != null ? Collections.singleton(this.contentLength.toString()) :
-					Collections.emptySet();
+		if (this.contentLength != null && HttpHeaders.CONTENT_LENGTH.equalsIgnoreCase(name)) {
+			return Collections.singleton(this.contentLength.toString());
 		}
-		else if (HttpHeaders.CONTENT_TYPE.equalsIgnoreCase(name)) {
-			return this.contentType != null ? Collections.singleton(this.contentType) : Collections.emptySet();
+		else if (this.contentType != null && HttpHeaders.CONTENT_TYPE.equalsIgnoreCase(name)) {
+			return Collections.singleton(this.contentType);
 		}
 		else {
 			return super.getHeaders(name);
@@ -330,7 +333,7 @@ public class ContentCachingResponseWrapper extends HttpServletResponseWrapper {
 					}
 					this.contentLength = null;
 				}
-				if (complete || this.contentType != null) {
+				if (this.contentType != null) {
 					rawResponse.setContentType(this.contentType);
 					this.contentType = null;
 				}

spring-web/src/test/java/org/springframework/web/filter/ContentCachingResponseWrapperTests.java
@@ -19,6 +19,7 @@ package org.springframework.web.filter;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.http.MediaType;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.testfixture.servlet.MockHttpServletResponse;
 import org.springframework.web.util.ContentCachingResponseWrapper;
@@ -26,12 +27,14 @@ import org.springframework.web.util.ContentCachingResponseWrapper;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.CONTENT_LENGTH;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.HttpHeaders.TRANSFER_ENCODING;
 
 /**
  * Tests for {@link ContentCachingResponseWrapper}.
  *
  * @author Rossen Stoyanchev
+ * @author Sam Brannen
  */
 class ContentCachingResponseWrapperTests {
 
@@ -48,6 +51,79 @@ class ContentCachingResponseWrapperTests {
 		assertThat(response.getStatus()).isEqualTo(HttpServletResponse.SC_OK);
 		assertThat(response.getContentLength()).isGreaterThan(0);
 		assertThat(response.getContentAsByteArray()).isEqualTo(responseBody);
+	}
+
+	@Test
+	void copyBodyToResponseWithPresetHeaders() throws Exception {
+		String PUZZLE = "puzzle";
+		String ENIGMA = "enigma";
+		String NUMBER = "number";
+		String MAGIC = "42";
+
+		byte[] responseBody = "Hello World".getBytes(UTF_8);
+		String responseLength = Integer.toString(responseBody.length);
+		String contentType = MediaType.APPLICATION_JSON_VALUE;
+
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		response.setContentType(contentType);
+		response.setContentLength(999);
+		response.setHeader(PUZZLE, ENIGMA);
+		response.setIntHeader(NUMBER, 42);
+
+		ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+		responseWrapper.setStatus(HttpServletResponse.SC_OK);
+
+		assertThat(responseWrapper.getStatus()).isEqualTo(HttpServletResponse.SC_OK);
+		assertThat(responseWrapper.getContentSize()).isZero();
+		assertThat(responseWrapper.getHeaderNames())
+				.containsExactlyInAnyOrder(PUZZLE, NUMBER, CONTENT_TYPE, CONTENT_LENGTH);
+
+		assertThat(responseWrapper.containsHeader(PUZZLE)).as(PUZZLE).isTrue();
+		assertThat(responseWrapper.getHeader(PUZZLE)).as(PUZZLE).isEqualTo(ENIGMA);
+		assertThat(responseWrapper.getHeaders(PUZZLE)).as(PUZZLE).containsExactly(ENIGMA);
+
+		assertThat(responseWrapper.containsHeader(NUMBER)).as(NUMBER).isTrue();
+		assertThat(responseWrapper.getHeader(NUMBER)).as(NUMBER).isEqualTo(MAGIC);
+		assertThat(responseWrapper.getHeaders(NUMBER)).as(NUMBER).containsExactly(MAGIC);
+
+		assertThat(responseWrapper.containsHeader(CONTENT_TYPE)).as(CONTENT_TYPE).isTrue();
+		assertThat(responseWrapper.getHeader(CONTENT_TYPE)).as(CONTENT_TYPE).isEqualTo(contentType);
+		assertThat(responseWrapper.getHeaders(CONTENT_TYPE)).as(CONTENT_TYPE).containsExactly(contentType);
+		assertThat(responseWrapper.getContentType()).as(CONTENT_TYPE).isEqualTo(contentType);
+
+		assertThat(responseWrapper.containsHeader(CONTENT_LENGTH)).as(CONTENT_LENGTH).isTrue();
+		assertThat(responseWrapper.getHeader(CONTENT_LENGTH)).as(CONTENT_LENGTH).isEqualTo("999");
+		assertThat(responseWrapper.getHeaders(CONTENT_LENGTH)).as(CONTENT_LENGTH).containsExactly("999");
+
+		FileCopyUtils.copy(responseBody, responseWrapper.getOutputStream());
+		responseWrapper.copyBodyToResponse();
+
+		assertThat(responseWrapper.getHeaderNames())
+				.containsExactlyInAnyOrder(PUZZLE, NUMBER, CONTENT_TYPE, CONTENT_LENGTH);
+
+		assertThat(response.getStatus()).isEqualTo(HttpServletResponse.SC_OK);
+		assertThat(response.getContentType()).isEqualTo(contentType);
+		assertThat(response.getContentLength()).isEqualTo(responseBody.length);
+		assertThat(response.getContentAsByteArray()).isEqualTo(responseBody);
+		assertThat(response.getHeaderNames())
+				.containsExactlyInAnyOrder(PUZZLE, NUMBER, CONTENT_TYPE, CONTENT_LENGTH);
+
+		assertThat(response.containsHeader(PUZZLE)).as(PUZZLE).isTrue();
+		assertThat(response.getHeader(PUZZLE)).as(PUZZLE).isEqualTo(ENIGMA);
+		assertThat(response.getHeaders(PUZZLE)).as(PUZZLE).containsExactly(ENIGMA);
+
+		assertThat(response.containsHeader(NUMBER)).as(NUMBER).isTrue();
+		assertThat(response.getHeader(NUMBER)).as(NUMBER).isEqualTo(MAGIC);
+		assertThat(response.getHeaders(NUMBER)).as(NUMBER).containsExactly(MAGIC);
+
+		assertThat(response.containsHeader(CONTENT_TYPE)).as(CONTENT_TYPE).isTrue();
+		assertThat(response.getHeader(CONTENT_TYPE)).as(CONTENT_TYPE).isEqualTo(contentType);
+		assertThat(response.getHeaders(CONTENT_TYPE)).as(CONTENT_TYPE).containsExactly(contentType);
+		assertThat(response.getContentType()).as(CONTENT_TYPE).isEqualTo(contentType);
+
+		assertThat(response.containsHeader(CONTENT_LENGTH)).as(CONTENT_LENGTH).isTrue();
+		assertThat(response.getHeader(CONTENT_LENGTH)).as(CONTENT_LENGTH).isEqualTo(responseLength);
+		assertThat(response.getHeaders(CONTENT_LENGTH)).as(CONTENT_LENGTH).containsExactly(responseLength);
 	}
 
 	@Test