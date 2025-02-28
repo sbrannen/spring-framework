68989322d158561ac7c2d16caba3923509717163
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2025-02-28 16:51:36 +0100

    Introduce RuntimeHints support in AotContextLoader
    This commit introduces a new loadContextForAotProcessing(...) variant
    in AotContextLoader which accepts a RuntimeHints argument. This new
    method is an interface default method which delegates to the existing
    loadContextForAotProcessing(MergedContextConfiguration) variant for
    backward compatibility.
    
    In addition, the original loadContextForAotProcessing(...) variant is
    now deprecated and has been converted to an interface default method
    which throws an UnsupportedOperationException.
    
    Note, however, that the framework now only invokes the new
    loadContextForAotProcessing(...) variant within TestContextAotGenerator.
    
    Closes gh-34513

--- spring-test/src/test/java/org/springframework/test/context/support/DelegatingSmartContextLoaderTests.java
+++ spring-test/src/test/java/org/springframework/test/context/support/DelegatingSmartContextLoaderTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -178,6 +178,7 @@ class DelegatingSmartContextLoaderTests {
 			assertApplicationContextLoadsForAotProcessing(mergedConfig, "ConfigClassTestCase.Config");
 		}
 
+		@SuppressWarnings("removal")
 		private void assertApplicationContextLoadsForAotProcessing(MergedContextConfiguration mergedConfig,
 				String expectedBeanDefName) throws Exception {
 
