275e17aecbf59d92239b13170ff592c65cb7b4a0
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2025-05-19 13:22:02 +0200

    Modify example to use @⁠Configuration and @⁠ImportResource
    See gh-34909

--- spring-test/src/test/java/example/XmlPlaceholderTests.java
+++ spring-test/src/test/java/example/XmlPlaceholderTests.java
@@ -3,6 +3,8 @@ package example;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,6 +18,12 @@ class XmlPlaceholderTests {
 	@Test
 	void test() {
 		assertThat(calculationService.getCalculator()).isExactlyInstanceOf(BasicCalculator.class);
+	}
+
+
+	@Configuration
+	@ImportResource("classpath:example/appContext.xml")
+	static class Config {
 	}
 
 }
